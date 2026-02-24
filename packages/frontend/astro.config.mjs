bd86a5394c10d3e13dd2304b7cdbf83571964492
Alex Dombroski <alexdombroski1@gmail.com>
2026-02-24 13:50:18 -0700

    feat: add static deploy to github pages

--- packages/frontend/astro.config.mjs
+++ packages/frontend/astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
   integrations: [svelte()],
+  vite: {
+    build: {
+      emptyOutDir: true,
+    },
+  },
 });
