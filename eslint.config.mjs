c74c87f9735a4e246fffd135709100862557effd
Alex Dombroski <alexdombroski1@gmail.com>
2026-02-27 11:12:31 -0700

    feat(dx): require svelte components used in astro files to be client based

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,53 @@ import astro from "eslint-plugin-astro";
 import svelte from "eslint-plugin-svelte";
 import svelteParser from "svelte-eslint-parser";
 
+/**
+ * @type {import("eslint").ESLint.Plugin}
+ */
+const noStaticSvelte = {
+  meta: {
+    name: "No Static Svelte",
+  },
+  rules: {
+    "no-static-svelte": {
+      meta: {
+        type: "problem",
+      },
+      create(context) {
+        const svelteComponents = new Set();
+        const validProperties = new Set(["load", "idle", "visible"]);
+
+        return {
+          ImportDeclaration(node) {
+            if (node.source.value.endsWith(".svelte")) {
+              node.specifiers.forEach((specifier) => {
+                svelteComponents.add(specifier.local.name);
+              });
+            }
+          },
+          JSXOpeningElement(node) {
+            const name = node.name.name;
+            if (!svelteComponents.has(name)) return;
+            const hasClientDirective = node.attributes.some((attr) => {
+              return (
+                attr.name.namespace?.name === "client" &&
+                validProperties.has(attr.name.name?.name)
+              );
+            });
+
+            if (!hasClientDirective) {
+              context.report({
+                node,
+                message: `Svelte component "${name}" is missing a client directive`,
+              });
+            }
+          },
+        };
+      },
+    },
+  },
+};
+
 export default defineConfig([
   {
     ignores: ["**/node_modules/**", "**/dist/**", "**/.astro/**"],
@@ -60,6 +107,15 @@ export default defineConfig([
     },
   },
   ...astro.configs.recommended,
+  {
+    files: ["**/*.astro"],
+    plugins: {
+      custom: noStaticSvelte,
+    },
+    rules: {
+      "custom/no-static-svelte": "warn",
+    },
+  },
   {
     files: ["**/*.svelte", "**/*.svelte.ts"],
     languageOptions: {
